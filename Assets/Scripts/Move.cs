addb6b655ea9279600536833d8b6d4bf40b713cf
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2024-04-30 17:58:05 -0500

    Con Animación

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -8,11 +8,14 @@ public class Move : MonoBehaviour
     [SerializeField] private string _Horizontal;
     [SerializeField] private string _Vertical;
     [SerializeField] private float _Speed;
+    [SerializeField] private Animator _animator;
     //[SerializeField] private string _key;
     private Rigidbody2D _rb;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -22,9 +25,20 @@ public class Move : MonoBehaviour
         float ver = Input.GetAxisRaw(_Vertical);
 
         _rb.velocity = new Vector2(Hor, ver)*_Speed;
+        _animator.SetFloat("Velx", Hor);
+
+        if(Hor < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+
+        }
 
 
-            if (Input.GetKey(KeyCode.Return))
+        if (Input.GetKey(KeyCode.Return))
             {
                 float velocity = 3f;
                 Vector2 move = _rb.velocity* velocity;
