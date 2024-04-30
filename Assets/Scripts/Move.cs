a1bc01da7945f79a177984afe5aae7de20677f5a
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2024-04-30 09:50:29 -0500

    prueba

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private string _Horizontal;
+    [SerializeField] private string _Vertical;
+    [SerializeField] private float _Speed;
+    //[SerializeField] private string _key;
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float Hor = Input.GetAxisRaw(_Horizontal);
+        float ver = Input.GetAxisRaw(_Vertical);
+
+        _rb.velocity = new Vector2(Hor, ver)*_Speed;
+
+
+            if (Input.GetKey(KeyCode.Return))
+            {
+                float velocity = 3f;
+                Vector2 move = _rb.velocity* velocity;
+                _rb.AddForce(move, ForceMode2D.Impulse);
+            }
+
+        
+
+
+    }
+}
